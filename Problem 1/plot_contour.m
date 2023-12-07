1ef0f9b250bfa6ac326e99f702c86595ddfc7685
Nikos Stylianou <stylianou404@gmail.com>
2023-12-07 23:32:33 +0200

    Updated the figures
    Co-Authored-By: Gianni-Alexandra <152874030+Gianni-Alexandra@users.noreply.github.com>

--- Problem 1/plot_contour.m
+++ Problem 1/plot_contour.m
@@ -3,7 +3,7 @@ function [Z] = plot_contour(start_num, end_num)
     x = linspace(start_num, end_num, 100);
     y = x;
     [X, Y] = meshgrid(x, y);
-    Z = X.^2 + 4*x.*Y + Y.^2;
+    Z = X.^2 + 4*X.*Y + Y.^2;
     contour(X, Y, Z, 40);
     xlabel('X');
     ylabel('Y');
