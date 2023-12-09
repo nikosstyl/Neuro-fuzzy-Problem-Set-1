945b41f6c5a6841ec91522d8620f8342f1eca871
Gianni-Alexandra <algianni@uth.gr>
2023-12-10 01:40:15 +0200

    Added 3D contour

--- Problem 1/plot_contour.m
+++ Problem 1/plot_contour.m
@@ -4,9 +4,13 @@ function [Z] = plot_contour(start_num, end_num)
     y = x;
     [X, Y] = meshgrid(x, y);
     Z = X.^2 + 4*X.*Y + Y.^2;
-    contour(X, Y, Z, 40);
+    figure(1);
+    contour3(X, Y, Z, 50);
+    set(gca, "FontName", "Linux Libertine O");
+    figure(2);
+    contour(X, Y, Z, 50);
+    set(gca, "FontName", "Linux Libertine O");
     xlabel('X');
     ylabel('Y');
-    % heatmap(Z);
 
 end
