a6d03f1239194cb76b90255496d27d84984f8e1e
Nikos Stylianou <stylianou404@gmail.com>
2023-12-10 23:00:35 +0200

    Translated to english everything
    Also, added plots in prob1

--- Problem 1/plot_contour.m
+++ Problem 1/plot_contour.m
@@ -6,10 +6,12 @@ function [Z] = plot_contour(start_num, end_num)
     Z = X.^2 + 4*X.*Y + Y.^2;
     figure(1);
     contour3(X, Y, Z, 50);
-    set(gca, "FontName", "Linux Libertine O");
+    colorbar;
+    set(gca, "FontName", "Times New Roman");
     figure(2);
     contour(X, Y, Z, 50);
-    set(gca, "FontName", "Linux Libertine O");
+    colorbar;
+    % set(gca, "FontName", "Linux Libertine O");
     xlabel('X');
     ylabel('Y');
 
