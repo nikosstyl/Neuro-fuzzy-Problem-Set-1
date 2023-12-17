416df377d41525d4b097fce91bdb7598f0543448
Gianni-Alexandra <algianni@uth.gr>
2023-12-18 00:38:11 +0200

    problem8_finished

--- Problem 8/eigenvalue_R.m
+++ Problem 8/eigenvalue_R.m
@@ -3,7 +3,11 @@ R = [12.4 7.6;
     7.6 6.4];
 
 %Compute the eigenvalues of R matrix
-eigenvalues = eig(R);
+eigenvalues = eig(R)
 
 % Find the maximum eigenvalue
-lambda_max = max(eigenvalues)
+lambda_max = max(eigenvalues);
+
+%Find maximum learning rate
+lr = 1/lambda_max
+
